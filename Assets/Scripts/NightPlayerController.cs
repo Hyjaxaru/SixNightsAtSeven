f3ecdc56b7f1978c707b1a10bfea9d272967c450
Hyjaxaru <noah.albrock@gmail.com>
2025-12-05 11:44:29 +0000

    CameraUI & Minimap
    The camera system object now flips up on button press
    
    The camera system UI has been created, but still needs to be finished
    
    I've also been experimenting with how to display the minimap. I've drawn it in Affinity now (source file included) but I did look into render textures

--- Assets/Scripts/NightPlayerController.cs
+++ Assets/Scripts/NightPlayerController.cs
@@ -18,59 +18,102 @@ public class NightPlayerController : MonoBehaviour
     [Range(0, 1)] public float officeMoveDuration = 0.5f;
     
     // the flashlight
-    [Header("Flashlight")]
+    [Header("The Flashlight")]
     public Light flashlight;
+    
+    // the camera view
+    [Header("The Cameras")]
+    public GameObject cameraMonitor;
+    
+    // the up and down transforms for the monitor to move between
+    public Transform cameraTransformEnabled;
+    public Transform cameraTransformDisabled;
+    
+    // the state of the camera being up or down
+    public bool isCameraOpen;
+    
+    // the speed at which the camera's open
+    [Range(0, 1)] public float cameraToggleDuration = 0.5f;
+    
+    // --- Office Movement --- //
 
-    private IEnumerator MoveOfficeCameraPosition()
+    private IEnumerator MoveOfficeCamera()
     {
-        // get the start and end position of the movement
-        Vector3 origin = transform.position;
-        Vector3 target = officeCameraTransforms[currentOfficeIndex].position;
+        // get the origin and target transforms for the player
+        var positionOrigin = transform.position;
+        var rotationOrigin = transform.rotation;
+        
+        var positionTarget = officeCameraTransforms[currentOfficeIndex].position;
+        var rotationTarget = officeCameraTransforms[currentOfficeIndex].rotation;
         
         // begin moving te camera
-        float elapsed = 0.0f;
+        var elapsed = 0.0f;
         while (elapsed < officeMoveDuration)
         {
-            transform.position = Vector3.Lerp(origin, target, Mathf.SmoothStep(0.0f, 1.0f, elapsed / officeMoveDuration));
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-        transform.position = target;
-    }
-
-    private IEnumerator MoveOfficeCameraRotation()
-    {
-        // get the start and end rotation of the camera
-        Quaternion origin = transform.rotation;
-        Quaternion target = officeCameraTransforms[currentOfficeIndex].rotation;
-        
-        // start rotating the camera
-        float elapsed = 0.0f;
-        while (elapsed < officeMoveDuration)
-        {
-            transform.rotation = Quaternion.Lerp(origin, target, Mathf.SmoothStep(0.0f, 1.0f, elapsed / officeMoveDuration));
+            var amt = Mathf.SmoothStep(0.0f, 1.0f, elapsed / officeMoveDuration);
+            transform.position = Vector3.Lerp(positionOrigin, positionTarget, amt);
+            transform.rotation = Quaternion.Slerp(rotationOrigin, rotationTarget, amt);
             
             elapsed += Time.deltaTime;
             yield return null;
         }
-        transform.rotation = target;
-    } 
+        
+        transform.position = positionTarget;
+        transform.rotation = rotationTarget;
+    }
 
     void OnMove(InputValue inputValue)
     {
-        Vector2 value = inputValue.Get<Vector2>();
-        if (value != Vector2.zero)
-        {
-            var xInt = Mathf.CeilToInt(value.x);
-            currentOfficeIndex = Mathf.Clamp(currentOfficeIndex + xInt, 0, officeCameraTransforms.Count - 1);
+        var value = inputValue.Get<Vector2>();
+        if (value == Vector2.zero) return;
+        
+        var xInt = Mathf.CeilToInt(value.x);
+        currentOfficeIndex = Mathf.Clamp(currentOfficeIndex + xInt, 0, officeCameraTransforms.Count - 1);
             
-            StartCoroutine(MoveOfficeCameraPosition());
-            StartCoroutine(MoveOfficeCameraRotation());
-        }
+        StartCoroutine(MoveOfficeCamera());
     }
-
-    void OnFlash(InputValue inputValue)
+    
+    // --- The Flashlight --- //
+    
+    void OnFlash(InputValue _)
     {
         flashlight.enabled = !flashlight.enabled;
+    }
+    
+    // --- Cameras --- //
+
+    private IEnumerator ToggleCameras(bool newState)
+    {
+        // if the desired state is the same as the current, do nothing
+        if (newState == isCameraOpen) yield break;
+        
+        // decide the origin and target transforms
+        var positionOrigin = cameraMonitor.transform.position;
+        var rotationOrigin = cameraMonitor.transform.rotation;
+        
+        // the target should be the oppisite o
+        var transformTarget = newState ? cameraTransformEnabled : cameraTransformDisabled;
+        var positionTarget = transformTarget.position;
+        var rotationTarget = transformTarget.rotation;
+        
+        // start moving the monitor
+        var elapsed = 0.0f;
+        while (elapsed < cameraToggleDuration)
+        {
+            var amt = Mathf.SmoothStep(0.0f, 1.0f, elapsed / cameraToggleDuration);
+            cameraMonitor.transform.position = Vector3.Lerp(positionOrigin, positionTarget, amt);
+            cameraMonitor.transform.rotation = Quaternion.Slerp(rotationOrigin, rotationTarget, amt);
+            
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        
+        // once everything has moved, declare that the camera has changed
+        isCameraOpen = newState;
+    }
+
+    void OnViewCameras(InputValue _)
+    {
+        StartCoroutine(ToggleCameras(!isCameraOpen));
     }
 }
