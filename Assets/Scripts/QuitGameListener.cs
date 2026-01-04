e24158d852ad038688852c74c591acc59ff71bd7
Hyjaxaru <noah.albrock@gmail.com>
2026-01-04 16:32:01 +0000

    Update QuitGameListener.cs
    QuitGameListener is now global and will not destroy on load

--- Assets/Scripts/QuitGameListener.cs
+++ Assets/Scripts/QuitGameListener.cs
@@ -3,6 +3,22 @@ using UnityEngine.InputSystem;
 
 public class QuitGameListener : MonoBehaviour
 {
+    public QuitGameListener Instance;
+    
+    void Awake()
+    {
+        // destroy if more than once
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        // make singleton
+        Instance = this;
+        DontDestroyOnLoad(Instance);
+    }
+    
     void OnQuitGame()
     {
         #if UNITY_EDITOR
